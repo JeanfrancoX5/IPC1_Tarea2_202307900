b444980e6f2929bc3c56064d2430fb2ad16ba83c
JeanfrancoX5 <jonathanchirix@gmail.com>
2024-02-16 22:11:46 -0600

    Entrega Tarea 2

---
+++ Tarea2_202307900/src/tarea2_202307900/Tarea2_202307900.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarea2_202307900;
+
+/**
+ *
+ * @author Jenfranco Chirix
+ */
+public class Tarea2_202307900 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
